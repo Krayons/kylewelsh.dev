f2f1d81c16bc9df746b2ceb8aeec6408baa7ae0e
“kyle” <kyle@hifranc.com>
2022-01-24 12:29:54 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kylewelsh.dev
+
 My personal website
---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col h-screen justify-between">
+
+    <!--Container-->
+    <div class="container w-full md:max-w-3xl mx-auto pt-20">
+
+      <div class="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
+
+        <!--Title-->
+        <div class="font-sans">
+          <h1 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">Welcome </h1>
+        </div>
+
+        <!--Post Content-->
+
+        <!--Lead Para-->
+        <p class="py-6">
+          👋 Welcome, I am hoping to write out finance and tech.
+        </p>
+
+        <p class="py-6">Part of my new year's resolutions (2022) is to improve my writing, so I'll be publishing
+          one article a I gave myself January to setup this site :)</p>
+
+        <!--/ Post Content-->
+
+      </div>
+    </div>
+    <!--/container-->
+
+    <footer class="bg-white border-t border-gray-400 shadow">
+      <div class="container max-w-4xl mx-auto flex py-8">
+
+        <div class="w-full mx-auto flex flex-wrap">
+          <div class="flex w-full md:w-1/2 ">
+            <div class="px-8">
+              <h3 class="font-bold text-gray-900">About</h3>
+              <p class="py-4 text-gray-600 text-sm">
+                FinTech, Software Engineer currently based in Cape Town /--\
+              </p>
+            </div>
+          </div>
+
+          <div class="flex w-full md:w-1/2">
+            <div class="px-8">
+              <h3 class="font-bold text-gray-900">Social</h3>
+              <ul class="list-reset items-center text-sm pt-3">
+                <li>
+                  <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1"
+                     href="https://twitter.com/KyleWelsh">Twitter</a>
+                </li>
+                <li>
+                  <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1"
+                     href="https://www.linkedin.com/in/kyle-ian-bransby-welsh/">LinkedIn</a>
+                </li>
+                <li>
+                  <a class="inline-block text-gray-600 no-underline hover:text-gray-900 hover:text-underline py-1"
+                     href="https://www.instagram.com/krayonsza/">Instagram</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </footer>
+
+  </div>
+</template>
+
+<style></style>
